a83a3c16d9f0648569664d15482791e7f2f31ef4
Alex Chien <alex.chien@koocaa.com>
2017-09-14 02:10:34 +0800

    secret token

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Acgdp::Application.config.secret_key_base = '5b98630ef6a624f233a1a4959a5030cbdf9b8d60417a16e5d2860f29c878133cd9268044ea32c368d7c54633c330adae92e31a0e69d62f73140d45278f6cb626'
